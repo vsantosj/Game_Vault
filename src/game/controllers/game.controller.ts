0f9f1f837849a348ba600358bdb90abf3d9d0429
viviane santos <vjsantos.viviane@gmail.com>
2025-08-05 23:38:34 -0300

    feat(game): implement GameService with CRUD operations

--- src/game/controllers/game.controller.ts
+++ src/game/controllers/game.controller.ts
@@ -1,9 +1,46 @@
-import { Controller } from "@nestjs/common";
+import { DeleteResult, UpdateDateColumn } from 'typeorm';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put } from "@nestjs/common";
 import { GameService } from "../services/game.service";
+import { Game } from "../entities/game.entity";
 
 
 @Controller('/games')
-export class GameController{
-    
-    constructor( private readonly gameService: GameService){}
+export class GameController {
+
+    constructor(private readonly gameService: GameService) { }
+
+
+    @Get()
+    @HttpCode(HttpStatus.OK)
+    findAll(): Promise<Game[]> {
+        return this.gameService.findAll();
+    }
+
+    @Get('/:id')
+    findById(@Param('id', ParseIntPipe) id: number): Promise<Game> {
+        return this.gameService.findById(id);
+    }
+
+    @Get('/categoria/:id')
+    getGameByCategory(@Param('id', ParseIntPipe) id: number): Promise<Game[]> {
+        return this.gameService.getGameByCategory(id);
+    }
+
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    create(@Body() game: Game): Promise<Game> {
+        return this.gameService.create(game)
+    }
+
+    @Put('/:id')
+    @HttpCode(HttpStatus.OK)
+    update(@Param('id',ParseIntPipe)id: number, @Body()game:Game): Promise<Game>{
+        return this.gameService.update(game);
+    }
+
+
+    @Delete('/:id')
+    delete(@Param('id', ParseIntPipe) id: number): Promise<DeleteResult> {
+        return this.gameService.delete(id);
+    }
 }
--- src/game/services/game.service.ts
+++ src/game/services/game.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable, NotImplementedException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
+import { DeleteResult, Repository } from "typeorm";
 import { CategoryService } from '../../category/services/category.service';
 import { Game } from '../entities/game.entity';
 
@@ -12,6 +12,60 @@ export class GameService{
         private gameRepository: Repository<Game>,
         private categoryService: CategoryService,
     ){}
-
     
+    async findAll(): Promise<Game[]>{
+        return await this.gameRepository.find({
+            relations:{
+                category: true
+            }
+        });
+    }
+
+    async findById(id: number): Promise<Game> {
+        const game = await this.gameRepository.findOne({
+            where:{id},
+            relations:{
+                category: true
+            }
+        });
+        if(!game)
+            throw new HttpException (`Game com  ID: ${id} não encontrado!`, HttpStatus.NOT_FOUND)
+
+        return game;
+    }
+
+        async getGameByCategory(id: number): Promise<Game[]>{
+            await this.categoryService.findById(id);
+
+            const games = await this.gameRepository.find({
+                where: {category: {id}},
+                relations:{
+                    category: true
+                }
+            });
+            if(games.length === 0) 
+                throw new NotImplementedException(`Nenhum game encontrado para a categoria com ID ${id}`)
+              return games;
+    }
+
+
+    async create(game: Game): Promise<Game>{
+        await this.categoryService.findById(game.category.id);
+        return await this.gameRepository.save(game);
+    }
+
+    async update(game: Game): Promise<Game>{
+
+        await this.findById(game.id);
+
+        await this.categoryService.findById(game.category.id);
+
+        return await this.gameRepository.save(game);
+    }
+
+
+    async delete(id: number): Promise<DeleteResult>{
+        await this.findById(id);
+        return await this.gameRepository.delete(id);
+    }
 }
